e2f58c44eac69c31cd4df023eabcb859c18cb71c
François Fortin <ff@fftstudios.com>
2013-05-07 07:32:06 -0400

    Changed CORS

--- src/main/java/com/uranos/ContactResource.java
+++ src/main/java/com/uranos/ContactResource.java
@@ -4,6 +4,8 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
 
 import org.apache.cxf.rs.security.cors.CrossOriginResourceSharing;
 
@@ -13,7 +15,7 @@ public class ContactResource {
 	@GET()
 	@CrossOriginResourceSharing(allowAllOrigins = true)
 	@Produces(MediaType.APPLICATION_JSON + "; charset=utf-8")
-	public Contact sayHello() {
+	public Response sayHello() {
 	  Contact c = new Contact();
 	  c.setNom("Stéphane");
 	  c.setPrenom("Le Cornec");
@@ -21,6 +23,9 @@ public class ContactResource {
 	  c.setRue("Jean-Talon");
 	  c.setVille("Montréal");
 	  c.setCodePostal("H1P3C3");
-	  return c;
+	  
+	  ResponseBuilder builder = Response.ok(c);
+	  builder.language("fr").header("Access-Control-Allow-Origin", "*");
+	  return builder.build();
 	}
 }
