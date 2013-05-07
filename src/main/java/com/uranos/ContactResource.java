bb95c003cf29464631499a48c70119a33ec38978
François Fortin <ff@fftstudios.com>
2013-05-07 10:57:55 -0400

    Bugfix

--- src/main/java/com/uranos/ContactResource.java
+++ src/main/java/com/uranos/ContactResource.java
@@ -4,18 +4,13 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.ResponseBuilder;
-
-import org.apache.cxf.rs.security.cors.CrossOriginResourceSharing;
 
 @Path("/contact")
 public class ContactResource {
 	
 	@GET()
-	@CrossOriginResourceSharing(allowAllOrigins = true)
 	@Produces(MediaType.APPLICATION_JSON + "; charset=utf-8")
-	public Response sayHello() {
+	public Contact sayHello() {
 	  Contact c = new Contact();
 	  c.setNom("Stéphane");
 	  c.setPrenom("Le Cornec");
@@ -23,9 +18,6 @@ public class ContactResource {
 	  c.setRue("Jean-Talon");
 	  c.setVille("Montréal");
 	  c.setCodePostal("H1P3C3");
-	  
-	  ResponseBuilder builder = Response.ok(c);
-	  builder.language("fr").header("Access-Control-Allow-Origin", "*");
-	  return builder.build();
+	  return c;
 	}
 }
